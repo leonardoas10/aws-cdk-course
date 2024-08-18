b94c00cf59ad7ed941d4f240284b8e0f18a7ee86
Leonardo Aranguren S <leoaranguren10@gmail.com>
2024-08-17 22:09:11 -0400

    Initial commit

---
+++ test/cdk-aws-course.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkAwsCourse from '../lib/cdk-aws-course-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-aws-course-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAwsCourse.CdkAwsCourseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
